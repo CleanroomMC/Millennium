d33fdb6ffe01fae1dda6b8d876f2c226ce4fd770
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2022-01-02 17:59:59 +0800

    Add settings for block

---
+++ src/main/java/io/github/cleanroommc/millennium/common/blocks/MillenniumBlock.java
@@ -0,0 +1,139 @@
+package io.github.cleanroommc.millennium.common.blocks;
+
+import java.util.function.Function;
+import javax.annotation.Nullable;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public abstract class MillenniumBlock extends Block {
+  Settings settings;
+
+  public MillenniumBlock(Settings settings) {
+    super(settings.material, settings.material.getMaterialMapColor());
+    setSoundType(settings.soundType);
+  }
+
+  @Override
+  public boolean isOpaqueCube(IBlockState state) {
+    return settings.opaque;
+  }
+
+  @Override
+  public boolean isCollidable() {
+    return settings.collidable;
+  }
+
+  @Override
+  public float getSlipperiness(
+      IBlockState state, IBlockAccess world, BlockPos pos, @Nullable Entity entity) {
+    return settings.slipperiness.apply(state, world, pos);
+  }
+
+  @Override
+  public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+    return settings.lightValue.apply(state, world, pos);
+  }
+
+  @Override
+  public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+    return settings.mapColor != null
+        ? settings.mapColor.apply(state, worldIn, pos)
+        : settings.material.getMaterialMapColor();
+  }
+
+  @FunctionalInterface
+  public interface ContextFunction<T1, T2, T3, R> {
+    R apply(T1 t1, T2 t2, T3 t3);
+  }
+
+  public static class Settings {
+    final Material material;
+    final ContextFunction<IBlockState, IBlockAccess, BlockPos, MapColor> mapColor;
+    boolean collidable = true;
+    boolean opaque = true;
+    SoundType soundType = SoundType.STONE;
+    ContextFunction<IBlockState, IBlockAccess, BlockPos, Integer> lightValue =
+        (state, access, pos) -> 0;
+    ContextFunction<IBlockState, IBlockAccess, BlockPos, Float> slipperiness =
+        (state, access, pos) -> 0.6F;
+
+    public Settings(Material material) {
+      this(material, material.getMaterialMapColor());
+    }
+
+    public Settings(Material material, MapColor mapColor) {
+      this(material, (state, access, pos) -> mapColor);
+    }
+
+    public Settings(
+        Material material,
+        ContextFunction<IBlockState, IBlockAccess, BlockPos, MapColor> mapColor) {
+      this.material = material;
+      this.mapColor = mapColor;
+    }
+
+    public Settings noCollision() {
+      collidable = false;
+      opaque = false;
+      return this;
+    }
+
+    public Settings nonOpaque() {
+      opaque = false;
+      return this;
+    }
+
+    public Settings soundType(SoundType soundType) {
+      this.soundType = soundType;
+      return this;
+    }
+
+    public Settings lightValue(
+        ContextFunction<IBlockState, IBlockAccess, BlockPos, Integer> lightValue) {
+      this.lightValue = lightValue;
+      return this;
+    }
+
+    public Settings lightValue(Function<IBlockState, Integer> lightValue) {
+      this.lightValue = (state, access, pos) -> lightValue.apply(state);
+      return this;
+    }
+
+    public Settings lightValue(int lightValue) {
+      this.lightValue = (state, access, pos) -> lightValue;
+      return this;
+    }
+
+    public Settings slipperiness(
+        ContextFunction<IBlockState, IBlockAccess, BlockPos, Float> slipperiness) {
+      this.slipperiness = slipperiness;
+      return this;
+    }
+
+    public Settings slipperiness(Function<IBlockState, Float> slipperiness) {
+      this.slipperiness = (state, access, pos) -> slipperiness.apply(state);
+      return this;
+    }
+
+    public Settings slipperiness(float slipperiness) {
+      this.slipperiness = (state, access, pos) -> slipperiness;
+      return this;
+    }
+
+    public static Settings copy(MillenniumBlock block) {
+      Settings settings = new Settings(block.settings.material, block.blockMapColor);
+      settings.collidable = block.settings.collidable;
+      settings.opaque = block.settings.opaque;
+      settings.soundType = block.settings.soundType;
+      settings.lightValue = block.settings.lightValue;
+      settings.slipperiness = block.settings.slipperiness;
+      return settings;
+    }
+  }
+}
