27ea9937e3727f022ab8bc3a1f7aa830ead543a5
jbredwards <47703524+jbredwards@users.noreply.github.com>
2022-05-13 08:55:58 -0400

    Update ScaffoldingBlock.java

--- src/main/java/com/cleanroommc/millennium/common/blocks/ScaffoldingBlock.java
+++ src/main/java/com/cleanroommc/millennium/common/blocks/ScaffoldingBlock.java
@@ -10,7 +10,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Optional
+import net.minecraftforge.fml.common.Optional;
 
 import javax.annotation.Nullable;
 import java.util.List;
