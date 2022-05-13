827e91eb5d32f6513e99609b258f9aea41a46e15
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-05-13 08:25:16 +0800

    minor code cleanup

--- src/main/java/com/cleanroommc/millennium/common/tag/TagDelegate.java
+++ src/main/java/com/cleanroommc/millennium/common/tag/TagDelegate.java
@@ -11,8 +11,8 @@ import java.util.stream.Stream;
  * A default implementation of ITaggable to help registry entries that don't want to provide it for themselves.
  */
 public class TagDelegate<T> implements ITaggable<T> {
-    private static final HashMap<ITaggable<?>, com.cleanroommc.millennium.common.tag.TagDelegate<?>> DELEGATES = new HashMap<>();
-    private static final HashMultimap<Tag, com.cleanroommc.millennium.common.tag.TagDelegate<?>> DELEGATES_HOLDING_TAG = HashMultimap.create();
+    private static final HashMap<ITaggable<?>, TagDelegate<?>> DELEGATES = new HashMap<>();
+    private static final HashMultimap<Tag, TagDelegate<?>> DELEGATES_HOLDING_TAG = HashMultimap.create();
     private final Class<T> delegateType;
     private final Set<Tag> tags = new HashSet<>();
 
@@ -20,8 +20,8 @@ public class TagDelegate<T> implements ITaggable<T> {
      * Do not use this TagDelegate instance for anything in your mod, use ITaggable directly.
      */
     @SuppressWarnings("unchecked")
-    public static <T> com.cleanroommc.millennium.common.tag.TagDelegate<T> getDelegate(ITaggable<T> taggable) {
-        return (com.cleanroommc.millennium.common.tag.TagDelegate<T>) DELEGATES.computeIfAbsent(taggable, t -> new com.cleanroommc.millennium.common.tag.TagDelegate<>(t.getTagDelegateType()));
+    public static <T> TagDelegate<T> getDelegate(ITaggable<T> taggable) {
+        return (TagDelegate<T>) DELEGATES.computeIfAbsent(taggable, t -> new com.cleanroommc.millennium.common.tag.TagDelegate<>(t.getTagDelegateType()));
     }
 
     /**
@@ -30,7 +30,7 @@ public class TagDelegate<T> implements ITaggable<T> {
      */
     public static void removeFromAll(Tag tag) {
         /* Create a new Set since delegates will be removed in the first place */
-        Set<com.cleanroommc.millennium.common.tag.TagDelegate<?>> delegates = new HashSet<>(DELEGATES_HOLDING_TAG.get(tag));
+        Set<TagDelegate<?>> delegates = new HashSet<>(DELEGATES_HOLDING_TAG.get(tag));
         delegates.forEach(delegate -> delegate.removeTag(tag));
     }
 
